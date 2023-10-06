09e20d2c317a0f8deb1446999cd27e4093e02fec
brenda <izabelibrenda817@gmail.com>
2023-10-06 09:40:09 -0400

    alal

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+
+{
+	static void Main()
+	{
+        Console.WriteLine("Digite um Número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite outro Número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num * num2;
+        if(resultado == 0) { Console.WriteLine("Número Inválido: "); }
+        if(resultado == 1) { Console.WriteLine("Número Inválido: "); }
+        if(resultado == 2) { Console.WriteLine("Número Inválido: "); }
+        if(resultado == 3) { Console.WriteLine("Número Inválido: "); }
+
+        Console.WriteLine($"O resultado dessa multiplicação é: {resultado}");
+
+        Console.ReadKey();
+    }
+}
